a19051c6159ea9def99142cef2b52c81ea7aa01f
Oicali <ilaciojairusmiguel@gmail.com>
2023-12-04 16:47:04 +0800

    Update first.java

---
+++ first.java
@@ -0,0 +1,9 @@
+public class first{
+    public static void main(String[]args){
+        getSmallest(3,7,2);
+    }
+
+    public static void getSmallest(int x, int y, int z){
+        System.out.println(Math.min(Math.min(x, y), z));
+    }
+}
